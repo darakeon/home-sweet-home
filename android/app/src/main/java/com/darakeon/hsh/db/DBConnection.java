e0334340abed4034995ce995dfd8013a1b847931
lucas_carol <lc.sales.duarte@gmail.com>
2017-07-02 07:07:51 -0300

    Filtering discarded places
    by [Keon] in [2016-09-07 18:21:29 (-03:00)]

--- android/app/src/main/java/com/darakeon/hsh/db/DBConnection.java
+++ android/app/src/main/java/com/darakeon/hsh/db/DBConnection.java
@@ -69,12 +69,10 @@ public class DBConnection extends SQLiteOpenHelper
 
         String[] projection = Place.C.GetAll();
 
-        //String selection = Place.C.COLUMN_NAME_DISCARDED + " = ?";
-        //String[] selectionArgs = { "0" };
+        String selection = Place.C.COLUMN_NAME_DISCARDED + " = ?";
+        String[] selectionArgs = { "0" };
 
-        //Cursor c = db.query(Place.C.TABLE_NAME, projection, selection, selectionArgs, null, null, null);
-
-        Cursor cursor = db.query(Place.C.TABLE_NAME, projection, null, null, null, null, null);
+        Cursor cursor = db.query(Place.C.TABLE_NAME, projection, selection, selectionArgs, null, null, null);
 
         cursor.moveToFirst();
         ArrayList<Place> placeList = new ArrayList<>();
